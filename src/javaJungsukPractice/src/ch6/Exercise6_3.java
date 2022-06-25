6fb295434b36df6718f7d4367b92d711e580f6f2
djdu4496 <djdu4496@gmail.com>
2022-06-25 23:57:27 +0900

    ch6 연습문제 6_3 ~ 6_4 완료

---
+++ src/javaJungsukPractice/src/ch6/Exercise6_3.java
@@ -0,0 +1,30 @@
+package ch6;
+
+/*
+ *	[6-3] 
+ *	다음과 같은 멤버변수를 갖는 Student클래스를 정의하시오.
+ *
+ *	타입          변수명          설명
+ *	String       name         학생이름
+ *  int          ban          반
+ *  int          no           번호
+ *  int          kor          국어점수
+ *  int          eng          영어점수 
+ *  int          math         수학점수
+ */
+
+class Student {
+	/**
+	 * 알맞은 코드를 넣어 완성하시오.
+	 */
+	String name;
+	int ban, no, kor, eng, math; 
+}
+
+class Exercise6_3 {
+
+	public static void main(String args[]) {
+
+
+	}
+}
---
+++ src/javaJungsukPractice/src/ch6/Exercise6_4.java
@@ -0,0 +1,58 @@
+package ch6;
+
+/*
+ *	[6-4] 
+ *	문제6-3에서 정의한 Student클래스에 
+ *	다음과 같이 정의된 두 개의 메서드 getTotal()과 getAverage()를 
+ *	추가하시오.
+ *
+ *	1. 메서드명 : getTotal
+ *     기   능 : 국어(kor), 영어(eng), 수학(math)의 점수를 모두 더해서 반환한다.
+ *     반환타입 : int
+ *     매개변수 : 없음
+ *	2. 메서드명 : getAverage
+ *     기   능 : 총점(국어점수+영어점수+수학점수)을 과목수로 나눈 평균을 구한다.
+ *     반환타입 : float 
+ *     매개변수 : 없음
+ *     
+ *  [실행결과]
+ *  이름:홍길동
+ * 	총점:236
+ *	평균:78.7
+ */
+
+class Student2 {
+	/**
+	 * 알맞은 코드를 넣어 완성하시오.
+	 */
+	String name = "홍길동";
+	int ban = 1, no = 1, kor = 100, eng = 60, math = 76; 
+	
+	int getTotal() {
+		return kor + eng + math;
+	}
+	
+	float getAverage() {
+		float average = getTotal() / (float)3;
+		average *= 10;
+		return Math.round((double)average) / (float)10;
+	}
+}
+
+class Exercise6_4 {
+
+	public static void main(String args[]) {
+		Student2 s = new Student2();
+		s.name = "홍길동"; 
+		s.ban  = 1;
+		s.no   = 1;
+		s.kor  = 100;
+		s.eng  = 60;
+		s.math = 76;
+		
+		System.out.println("이름:"+s.name);
+		System.out.println("총점:"+s.getTotal());
+		System.out.println("평균:"+s.getAverage());
+
+	}
+}
